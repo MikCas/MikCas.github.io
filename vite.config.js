023cdc9ba0330e5dd631c6fda9a6043991aa7bc9
MikCas <mikhail.cassar@gmail.com>
2026-04-17 15:21:28 +0200

    refactor: added css tokens, and restructed html and css for more brutalist layout

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home:      resolve(__dirname, 'index.html'),
-        strudel:   resolve(__dirname, 'projects/strudel-av.html'),
-        raytracer: resolve(__dirname, 'projects/raytracer.html'),
-        particles: resolve(__dirname, 'projects/particle-system.html'),
+        // strudel:   resolve(__dirname, 'projects/audiovisual.html'),
+        // raytracer: resolve(__dirname, 'projects/graphics.html'),
+        // particles: resolve(__dirname, 'projects/sculptures.html'),
       },
     },
   },
